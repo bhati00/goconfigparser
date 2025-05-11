bfb3ec09d1c8f752526d45e7c9a7a0184e54639b
princebhati <princebhati074@gmail.com>
2025-05-11 23:13:43 +0530

    refactor: enhance logging for config parsing errors and initialization

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -4,10 +4,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"goconfigparser/internal/models" // Adjust the import path as necessary
+	"log"
 	"strings"
 )
 
 func ParseConfig(fileContent string) ([]byte, error) {
+	log.Println("Parsing config file content")
 	// intialize the config struct
 	config := models.Config{
 		Sections: make(map[string]map[string]string),
@@ -27,6 +29,7 @@ func ParseConfig(fileContent string) ([]byte, error) {
 		if strings.HasPrefix(rawLine, "[") && strings.HasSuffix(rawLine, "]") {
 			currentSection = strings.TrimSpace(rawLine[1 : len(rawLine)-1])
 			if currentSection == "" {
+				log.Printf("section name is empty at line %d", lineNum+1)
 				return nil, fmt.Errorf("section name is empty at line %d", lineNum+1)
 			}
 			config.Sections[currentSection] = make(map[string]string)
@@ -36,22 +39,27 @@ func ParseConfig(fileContent string) ([]byte, error) {
 		// if it's a key-value pair
 		if strings.Contains(rawLine, "=") {
 			if currentSection == "" {
+				log.Printf("key-value pair found before section at line %d", lineNum+1)
 				return nil, fmt.Errorf("key-value pair found before section at line %d", lineNum+1)
 			}
 			parts := strings.SplitN(rawLine, "=", 2)
 			if len(parts) != 2 {
+				log.Printf("invalid key-value pair at line %d", lineNum+1)
 				return nil, fmt.Errorf("invalid key-value pair at line %d", lineNum+1)
 			}
 			key := strings.TrimSpace(parts[0])
 			value := strings.TrimSpace(parts[1])
 			if key == "" {
+				log.Printf("key is empty at line %d", lineNum+1)
 				return nil, fmt.Errorf("key is empty at line %d", lineNum+1)
 			}
 			if value == "" {
+				log.Printf("value is empty at line %d", lineNum+1)
 				return nil, fmt.Errorf("value is empty at line %d", lineNum+1)
 			}
 			config.Sections[currentSection][key] = value
 		} else {
+			log.Printf("invalid line format at line %d", lineNum+1)
 			return nil, fmt.Errorf("invalid line format at line %d", lineNum+1)
 		}
 	}
