0040e47e298fe1c9c39ebcce2690015f6dd694f4
princebhati <princebhati074@gmail.com>
2025-05-13 01:14:14 +0530

    refactor: update command-line flags for configuration input and output handling

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,8 @@ import (
 )
 
 func main() {
-	configPath := flag.String("file", "", "Path to the configuration file")
+	configPath := flag.String("input", "", "Path to the configuration file")
+	outPutPath := flag.String("output", "", "Path to the output file")
 	flag.Parse()
 	if *configPath == "" {
 		fmt.Println("Please provide a configuration file path using -file flag")
@@ -25,5 +26,31 @@ func main() {
 		os.Exit(1)
 	}
 
+	outPutDir := "output"
+	if *outPutPath != "" {
+		err = os.MkdirAll(outPutDir, os.ModePerm)
+		if err != nil {
+			fmt.Printf("Error creating output directory: %v\n", err)
+			os.Exit(1)
+		}
+	}
+	fullPath := fmt.Sprintf("%s/%s", outPutDir, *outPutPath)
+	if _, err := os.Stat(fullPath); err == nil {
+		fmt.Printf("File %s already exists. Overwrite? (y/n): ", fullPath)
+		var response string
+		fmt.Scanln(&response)
+		if response != "y" {
+			fmt.Println("Operation cancelled by user.")
+			os.Exit(0)
+		}
+	}
+
+	err = os.WriteFile(fullPath, []byte(config), 0644)
+	if err != nil {
+		fmt.Printf("Error writing to file: %v\n", err)
+		os.Exit(1)
+	} else {
+		fmt.Printf("File %s created successfully\n", fullPath)
+	}
 	fmt.Printf("Parsed config %s\n", config)
 }
